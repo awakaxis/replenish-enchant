c00a15a9da8cbb8125c3f55818d0f566614fb97b
awakaxis <ashton5350@gmail.com>
2024-03-04 22:03:46 -0800

    replenish now takes from loot tables if the loot table doesnt contain seeds--in the case of some modded crops--replenish will try to take from the player's inventory too

--- src/main/java/net/awakaxis/replenish/mixin/BlockMixin.java
+++ src/main/java/net/awakaxis/replenish/mixin/BlockMixin.java
@@ -1,5 +1,7 @@
 package net.awakaxis.replenish.mixin;
 
+import java.util.List;
+
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,32 +16,55 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 @Mixin(Block.class)
 public abstract class BlockMixin {
-    @Inject(method = "afterBreak", at = @At("RETURN"))
+    @Inject(method = "afterBreak", at = @At("HEAD"), cancellable = true)
     private void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack, CallbackInfo ci) {
-        Replenish.LOGGER.info("afterBreak mixin called");
-        if (((Block)(Object)this) instanceof CropBlock) {
-            CropBlock block = (CropBlock)(Object)this;
+        Block block = (Block)(Object)this;
+        List<ItemStack> drops = null;
+        boolean foundSeeds = false;
+        player.incrementStat(Stats.MINED.getOrCreateStat(block));
+        player.addExhaustion(0.005F);
+        if (world instanceof ServerWorld) {
+            drops = Block.getDroppedStacks(state, (ServerWorld) world, pos, blockEntity, player, stack);
+        }
+        if (block instanceof CropBlock) {
+            CropBlock block2 = (CropBlock)(Object)this;
             if (EnchantmentHelper.getLevel(Replenish.REPLENISH, player.getStackInHand(Hand.MAIN_HAND)) >= 1) {
-                Replenish.LOGGER.info("Replenish enchantment detected");
-                ItemStack itemStack = ItemStack.EMPTY;
-                for (int i = 0; i < player.getInventory().size(); ++i) {
-                    itemStack = player.getInventory().getStack(i);
-                    Replenish.LOGGER.info("stack name: " + itemStack.getItem().getName().getString() + " seed item name: " + ((CropBlockAccessor)block).invokeGetSeedsItem().asItem().getName().getString());
-                    if (itemStack.getItem() == ((CropBlockAccessor)block).invokeGetSeedsItem().asItem()) {
-                        break;
+                if (drops != null) {
+                    for (ItemStack drop : drops) {
+                        if (drop.getItem() == ((CropBlockAccessor)block).invokeGetSeedsItem().asItem()) {
+                            drop.decrement(1);
+                            foundSeeds = true;
+                            break;
+                        }
                     }
-                    itemStack = ItemStack.EMPTY;
                 }
-                if (itemStack.isEmpty()) return;
-                itemStack.decrement(1);
-                world.setBlockState(pos, block.withAge(0));
+                if (!foundSeeds) {
+                    ItemStack itemStack = ItemStack.EMPTY;
+                    for (int i = 0; i < player.getInventory().size(); ++i) {
+                        itemStack = player.getInventory().getStack(i);
+                        if (itemStack.getItem() == ((CropBlockAccessor)block).invokeGetSeedsItem().asItem()) {
+                            break;
+                        }
+                        itemStack = ItemStack.EMPTY;
+                    }
+                    if (itemStack.isEmpty()) return;
+                    itemStack.decrement(1);
+                }
+                world.setBlockState(pos, block2.withAge(0));
+            }
+            if (world instanceof ServerWorld) {
+                drops.forEach(_stack -> Block.dropStack(world, pos, _stack));
+                state.onStacksDropped((ServerWorld) world, pos, stack, true);
             }
         }
+        ci.cancel();
     }
 }
