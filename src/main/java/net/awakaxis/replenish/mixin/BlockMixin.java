c4c548417acc53f25de0db02e96208f4b3ef8f80
awakaxis <ashton5350@gmail.com>
2024-12-20 08:48:02 -0800

    minor change

--- src/main/java/net/awakaxis/replenish/mixin/BlockMixin.java
+++ src/main/java/net/awakaxis/replenish/mixin/BlockMixin.java
@@ -33,54 +33,55 @@ public abstract class BlockMixin {
     private void afterBreak(Level level, Player player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack, CallbackInfo ci) {
         Block block = (Block) (Object) this;
         // if using a replenish hoe on a crop
-        if (EnchantmentHelper.getItemEnchantmentLevel(ReplenishEnchantments.REPLENISH, stack) >= 1 && block instanceof CropBlock) {
-            List<ItemStack> drops = null;
-            boolean foundSeeds = false;
-            CropBlock block2 = (CropBlock)(Object)this;
-            player.awardStat(Stats.BLOCK_MINED.get(block));
-            player.causeFoodExhaustion(0.005F);
-            if (level instanceof ServerLevel serverLevel) {
-                // get the drop with more luck
-                LootParams.Builder builder = new LootParams.Builder(serverLevel)
-                    .withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(pos))
-                    .withParameter(LootContextParams.TOOL, stack)
-                    .withOptionalParameter(LootContextParams.THIS_ENTITY, player)
-                    .withOptionalParameter(LootContextParams.BLOCK_ENTITY, blockEntity);
-                drops = state.getDrops(builder);
-                // remove one seed from the drops
-                for (ItemStack drop : drops) {
-                    if (drop.getItem() == ((CropBlockAccessor)block).invokeGetBaseSeedId().asItem()) {
-                        drop.shrink(1);
-                        foundSeeds = true;
-                        break;
-                    }
-                }
-                // if the hoe is not diamond or better, remove half of the drops
-                if (stack.getItem() instanceof HoeItem hoe && hoe.getTier().getUses() < Tiers.DIAMOND.getUses()) {
+        if (stack.getItem() instanceof HoeItem hoe) {
+            if (EnchantmentHelper.getItemEnchantmentLevel(ReplenishEnchantments.REPLENISH, stack) >= 1 && block instanceof CropBlock cropBlock) {
+                List<ItemStack> drops = null;
+                boolean foundSeeds = false;
+                player.awardStat(Stats.BLOCK_MINED.get(block));
+                player.causeFoodExhaustion(0.005F);
+                if (level instanceof ServerLevel serverLevel) {
+                    // get the drop with more luck
+                    LootParams.Builder builder = new LootParams.Builder(serverLevel)
+                            .withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(pos))
+                            .withParameter(LootContextParams.TOOL, stack)
+                            .withOptionalParameter(LootContextParams.THIS_ENTITY, player)
+                            .withOptionalParameter(LootContextParams.BLOCK_ENTITY, blockEntity);
+                    drops = state.getDrops(builder);
+                    // remove one seed from the drops
                     for (ItemStack drop : drops) {
-                        drop.shrink(drop.getCount() / 2);
-                    }
-                }
-                // if there were no seeds in the loot, try to remove one from the player's inventory
-                if (!foundSeeds) {
-                    ItemStack itemStack = ItemStack.EMPTY;
-                    for (int i = 0; i < player.getInventory().getContainerSize(); ++i) {
-                        itemStack = player.getInventory().getItem(i);
-                        if (itemStack.getItem() == ((CropBlockAccessor)block).invokeGetBaseSeedId().asItem()) {
+                        if (drop.getItem() == ((CropBlockAccessor)block).invokeGetBaseSeedId().asItem()) {
+                            drop.shrink(1);
+                            foundSeeds = true;
                             break;
                         }
-                        itemStack = ItemStack.EMPTY;
                     }
-                    if (!itemStack.isEmpty()) foundSeeds = true;
-                    itemStack.shrink(1);
+                    // if the hoe is not diamond or better, remove half of the drops
+                    if (hoe.getTier().getUses() < Tiers.DIAMOND.getUses()) {
+                        for (ItemStack drop : drops) {
+                            drop.shrink(drop.getCount() / 2);
+                        }
+                    }
+                    // if there were no seeds in the loot, try to remove one from the player's inventory
+                    if (!foundSeeds) {
+                        ItemStack itemStack = ItemStack.EMPTY;
+                        for (int i = 0; i < player.getInventory().getContainerSize(); ++i) {
+                            itemStack = player.getInventory().getItem(i);
+                            if (itemStack.getItem() == ((CropBlockAccessor)block).invokeGetBaseSeedId().asItem()) {
+                                break;
+                            }
+                            itemStack = ItemStack.EMPTY;
+                        }
+                        if (!itemStack.isEmpty()) foundSeeds = true;
+                        itemStack.shrink(1);
+                    }
+                    if (foundSeeds) {
+                        level.setBlockAndUpdate(pos, cropBlock.getStateForAge(0));
+                    }
+                    drops.forEach(_stack -> Block.popResource(level, pos, _stack));
+                    state.spawnAfterBreak(serverLevel, pos, stack, true);
                 }
-                if (foundSeeds) {
-                    level.setBlockAndUpdate(pos, block2.getStateForAge(0));
-                }
-                drops.forEach(_stack -> Block.popResource(level, pos, _stack));
-                state.spawnAfterBreak(serverLevel, pos, stack, true);
+                ci.cancel();
             }
-            ci.cancel();
         }
     }
 }
